317320143c544a2de9a21f57d586f5d10e971045
韩世强 <brycehan@163.com>
2021-01-28 04:32:03 +0800

    Initial commit

---
+++ src/main/java/com/brycehan/boot/BryceBootApplication.java
@@ -0,0 +1,13 @@
+package com.brycehan.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BryceBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BryceBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brycehan/boot/BryceBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brycehan.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BryceBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
