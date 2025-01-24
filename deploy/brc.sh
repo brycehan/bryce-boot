a637939cdddb28aa23e33a0e3eb78a3e2c5e45ea
Bryce Han <brycehan@163.com>
2025-01-24 15:25:45 +0800

    perf(deploy): 优化应用启动脚本和日志配置
    - 更新 brc.sh 脚本，增加应用名称变量和启动状态检查
    - 修改 IndexController 中的欢迎信息
    - 更新 logback-spring.xml，增加日志滚动策略和详细配置

--- deploy/brc.sh
+++ deploy/brc.sh
@@ -2,43 +2,58 @@
 
 # 设置时区
 TZ=Asia/Shanghai
-# JVM参数
-JAVA_OPTS="-server -Xms1g -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m -XX:+HeapDumpOnOutOfMemoryError"
 
+# JVM参数
+JAVA_OPTS="-server -Xms1g -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -XX:+PrintGCDetails"
 JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
 JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=UTF-8"
 JAVA_OPTS="${JAVA_OPTS} -Duser.timezone=${TZ}"
 JAVA_OPTS="${JAVA_OPTS} -Dserver.port=8090"
 JAVA_OPTS="${JAVA_OPTS} -Dspring.profiles.active=prod"
 
+# 应用名称
+APP_NAME=bryce-boot-server-0.0.1-SNAPSHOT.jar
+
+# 启动脚本
 case $1 in
 "start") {
   echo "-------------------- 启动 bryce-boot --------------------"
-  # shellcheck disable=SC2086
-  nohup java -jar $JAVA_OPTS bryce-boot-server-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+  # shellcheck disable=SC2009
+  pid=$(ps -ef | grep java | grep $APP_NAME | grep -v grep | awk '{print $2}')
+  if [ -n "$pid" ]; then
+    echo "-------------------- bryce-boot 正在运行，pid: $pid --------------------"
+    exit 1
+    else
+      # shellcheck disable=SC2086
+      nohup java -jar $JAVA_OPTS $APP_NAME >/dev/null 2>&1 &
+      # shellcheck disable=SC2009
+      pid=$(ps -ef | grep java | grep $APP_NAME | grep -v grep | awk '{print $2}')
+      echo "-------------------- bryce-boot 启动成功，pid: $pid --------------------"
+      exit 1
+  fi
 };;
 
 "stop") {
   echo "-------------------- 停止 bryce-boot --------------------"
   # shellcheck disable=SC2009
-  ps -ef | grep bryce-boot | grep -v grep | awk '{print $2}' | xargs kill -9
+  ps -ef | grep java | grep $APP_NAME | grep -v grep | awk '{print $2}' | xargs kill -9
 };;
 
 "restart") {
   echo "-------------------- 重启 bryce-boot --------------------"
   # shellcheck disable=SC2009
-  pid=$(ps -ef | grep bryce-boot | grep -v grep | awk '{print $2}')
+  pid=$(ps -ef | grep java | grep $APP_NAME | grep -v grep | awk '{print $2}')
   if [ -n "$pid" ]; then
     kill -9 "$pid"
   fi
   # shellcheck disable=SC2086
-  nohup java -jar $JAVA_OPTS bryce-boot-server-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+  nohup java -jar $JAVA_OPTS $APP_NAME >/dev/null 2>&1 &
 };;
 
 "status") {
   echo "-------------------- bryce-boot 状态 --------------------"
   # shellcheck disable=SC2009
-  pid=$(ps -ef | grep bryce-boot | grep -v grep | awk '{print $2}')
+  pid=$(ps -ef | grep java | grep $APP_NAME | grep -v grep | awk '{print $2}')
   if [ -z "$pid" ]; then
     echo "-------------------- bryce-boot 未运行 --------------------"
   else
